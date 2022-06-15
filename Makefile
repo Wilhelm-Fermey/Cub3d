752d2b20ce6fc20ae25da1ee63044febdd822540
Wilhelm Fermey <wfermey@k2r4p9.42mulhouse.fr>
2022-06-15 17:00:07 +0200

    add all

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: wfermey <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/11 09:41:54 by wfermey           #+#    #+#              #
-#    Updated: 2022/06/15 13:43:26 by wfermey          ###   ########.fr        #
+#    Updated: 2022/06/15 16:43:17 by wfermey          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@ NAME =		cub3d
 FLAGS =		
 
 FILES = 	main.c parsing.c utils/utils1.c utils/get_next_line.c \
-			utils/split.c color.c
+			utils/split.c color.c map.c utils/utils2.c
 
 ${NAME}:
 			gcc -g ${FLAGS} ${FILES} -L/usr/local/lib -lmlx -framework OpenGL \
--- map.c
+++ map.c
@@ -6,59 +6,70 @@
 /*   By: wfermey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 13:58:54 by wfermey           #+#    #+#             */
-/*   Updated: 2022/06/15 14:33:04 by wfermey          ###   ########.fr       */
+/*   Updated: 2022/06/15 16:58:41 by wfermey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-/* ******************  malloc size of char**   ********************** */
+/* ******************  creat empty map   ********************** */
 char **	ft_size_map(char *str)
 {
 	int		fd;
 	int		i;
 	int		j;
 	char 	*tmp;
+	char	**map;
 
+	j = 0;
+	i = 0;
 	fd = open(str, O_RDONLY);
 	tmp = get_next_line(fd);
+	printf("tmp[i] ==  %c\n", tmp[i]);
 	while (tmp)
 	{
 		i = 0;
-		while (tmp[i] == ' ')
+		while (tmp[i] == 32)
 			i++;
-		if (str[i] == '1')
+		if (tmp[i] == '1')
 			j++;
+		tmp = get_next_line(fd);
 	}
 	printf("%d\n", j);
 	map = malloc(j + 1 * sizeof(char *));
-	return (mqp);
+	return (map);
 }
 
 /* ******************  put map in char**   ********************** */
-int	ft_put_map(char *str, t_file file)
+char	**ft_put_map(char *str, t_file *file)
 {
 	int		fd;
 	int		i;
+	int		j;
 	char 	*tmp;
 	char	**map;
 
+	j = 0;
 	map = ft_size_map(str);
+	/*
 	fd = open(str, O_RDONLY);
 	tmp = get_next_line(fd);
-	map = malloc(50 * sizeof(char *));
 	while (tmp)
 	{
 		i = 0;
 		while (tmp[i] == ' ')
 			i++;
-		if (str[i] == '1')
+		i++;
+		if (tmp[i] == '1')
 		{
 			map[j] = ft_strdup(tmp);
 			j++;
 		}
+		tmp = get_next_line(fd);
 	}
+	*/
 	map[j] = NULL;
+	return (map);
 }
 
 
@@ -69,4 +80,5 @@ int	ft_map(char *str, t_file *file)
 	char	**map;
 
 	map = ft_put_map(str, file);
+	return (0);
 }
